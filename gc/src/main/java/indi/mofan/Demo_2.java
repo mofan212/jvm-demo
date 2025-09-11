8b61ce62194ffb6de8abcfc260f9932e8fe0ccf8
mofan <cy.mofan@foxmail.com>
2025-09-11 16:56:26 +0800

    软引用与引用队列

--- gc/src/main/java/indi/mofan/Demo_2.java
+++ gc/src/main/java/indi/mofan/Demo_2.java
@@ -2,6 +2,8 @@ package indi.mofan;
 
 
 import java.io.IOException;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +20,7 @@ public class Demo_2 {
     private static final int _4MB = 4 * 1024 * 1024;
 
     public static void main(String[] args) throws IOException {
-        soft();
+        referenceQueue();
     }
 
     private static void method() throws IOException {
@@ -36,6 +38,31 @@ public class Demo_2 {
             System.out.println(ref.get());
             list.add(ref);
             System.out.println(list.size());
+        }
+
+        System.out.println("循环结束: " + list.size());
+        for (SoftReference<byte[]> reference : list) {
+            System.out.println(reference.get());
+        }
+    }
+
+    private static void referenceQueue() {
+        List<SoftReference<byte[]>> list = new ArrayList<>();
+        // 引用队列
+        ReferenceQueue<byte[]> queue = new ReferenceQueue<>();
+
+        for (int i = 0; i < 5; i++) {
+            // 关联引用队列。当软引用关联的 byte[] 被回收时，软引用自身会加入到引用队列里
+            SoftReference<byte[]> ref = new SoftReference<>(new byte[_4MB], queue);
+            System.out.println(ref.get());
+            list.add(ref);
+            System.out.println(list.size());
+        }
+
+        Reference<? extends byte[]> poll = queue.poll();
+        while (poll != null) {
+            list.remove(poll);
+            poll = queue.poll();
         }
 
         System.out.println("循环结束: " + list.size());
